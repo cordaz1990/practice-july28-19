6f3f2659579e113e107d45af5b20fd32dcd29a51
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:35:14 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
-print('hello')
-print('hello')
-print('world')
+def is_abecedarian(word):
+    previous = word[0]
+    for c in word:
+      if c < previous:
+        return False
+      previous = c
+    return True
+  
+  
+def is_abecedarian(word):
+    if len(word) <= 1:
+       return True
+    if word[0] >word[1]:
+       return False
+      
+    return is_abecedarian(word[1:])
